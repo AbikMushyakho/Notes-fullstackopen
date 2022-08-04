52f05addb65b9047710b9e45a5e8fb8889401806
AbikMushyakho <abik.mushyakho94@gmail.com>
2022-08-04 23:50:54 +0545

    rest client maintained

--- index.js
+++ index.js
@@ -8,34 +8,14 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "Frontend", "build")));
 const Note = require("./models/note");
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
 app.get("/", (request, response) => {
   response.sendFile(path.join(__dirname, "Frontend", "build", "index.html"));
 });
 
-app.get("/api/notes", (request, response) => {
+app.get("/api/notes", (request, response,next) => {
   Note.find({}).then((notes) => {
     response.json(notes);
-  });
+  }).catch(error => next(error));
 });
 app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
@@ -55,7 +35,7 @@ const generateId = () => {
   console.log(id);
   return id;
 };
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response,next) => {
   const body = request.body;
   if (body.content === null) {
     return response.status(400).json({
@@ -69,23 +49,29 @@ app.post("/api/notes", (request, response) => {
   });
   note.save().then((savedNote) => {
     response.json(savedNote);
-  });
+  }).catch(error => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response,next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 });
 
-app.put("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const data = request.body;
+app.put("/api/notes/:id", (request, response,next) => {
+  const body = request.body;
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
 
-  notes = notes.filter((note) => note.id !== id);
-  notes.concat(data);
-  response.status(201).json(data);
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 });
 
 
